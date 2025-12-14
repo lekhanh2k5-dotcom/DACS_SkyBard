ba5668fd3592423fab50c98809b03fd461734365
KhanhsLee <lekhanh2k5@gmail.com>
2025-12-14 18:00:32 +0700

    Link renderer.js to AppController

---
+++ code/src/view/renderer.js
@@ -0,0 +1,6 @@
+import { AppController } from "./controllers/AppController.js";
+
+document.addEventListener('DOMContentLoaded', () => {
+    const app = new AppController();
+    console.log("✅ SkyBard Interface Loaded!");
+});
